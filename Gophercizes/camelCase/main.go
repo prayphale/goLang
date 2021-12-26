756381f06942fc7cdad7b159fc29a189233e0fce
pooja-salucro <prayphale@salucro.com>
2021-12-26 16:28:26 +0530

    Added code for to check min & max characters in a string

--- Gophercizes/camelCase/main.go
+++ Gophercizes/camelCase/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var input string
 	fmt.Scanf("%s\n", &input)
+
+	answer := 1
+
+	for i, ch := range input {
+		fmt.Println("index:", i, "Run:", ch)
+
+		min, max := 'A', 'Z'
+
+		if ch >= min && ch <= max {
+			answer++
+		}
+
+		if ch < 69 && ch > 37 {
+			answer++
+		}
+
+		str := string(ch)
+		if strings.ToUpper(str) == str {
+			answer++
+		}
+
+	}
+
+	fmt.Println(answer)
 }
