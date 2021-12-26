8cd2a2471ce8edef6ff8ab33cecb80780dc0e485
pooja-salucro <prayphale@salucro.com>
2021-12-26 14:04:50 +0530

    Refactored code and created smaller functions to make function more clear and easy to debug.

--- urlShortener/handler.go
+++ urlShortener/handler.go
@@ -16,18 +16,29 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 }
 
 func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
-
-	var pathUrls []pathUrl
-	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	pathUrls, err := parseYaml(yamlBytes)
 	if err != nil {
 		return nil, err
 	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
 	pathsToUrls := make(map[string]string)
 	for _, pu := range pathUrls {
 		pathsToUrls[pu.path] = pu.URL
 	}
+	return pathsToUrls
+}
 
-	return MapHandler(pathsToUrls, fallback), nil
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
 }
 
 type pathUrl struct {
